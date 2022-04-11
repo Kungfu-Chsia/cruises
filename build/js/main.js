572ff012dd3f6ec03d93a4127767f245512f58dc
Kungfu-Chsia <allrighty@ya.ru>
2022-04-11 23:20:04 +0300

    новая сборка

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,185 @@
-import"./vendor.js";const formContactsInput=document.getElementById("telephone"),checkNumber=()=>{const t=formContactsInput.value.length;t<10?formContactsInput.setCustomValidity(`Ещё ${10-t} симв.`):t>12?formContactsInput.setCustomValidity(`Удалите лишние ${t-12} симв.`):Number.isInteger(+formContactsInput.value)?formContactsInput.setCustomValidity(""):formContactsInput.setCustomValidity("Номер должен быть числом"),formContactsInput.reportValidity()};null!==formContactsInput&&formContactsInput.addEventListener("input",checkNumber);const formContactsInputName=document.getElementById("name"),checkNumbers=()=>{const t=formContactsInputName.value.length;formContactsInputName.setCustomValidity(""),t>0&&t<1?formContactsInputName.setCustomValidity(`Ещё ${1-t} симв.`):t>45&&formContactsInputName.setCustomValidity(`Удалите лишние ${t-45} симв.`),t>0&&!/^[ A-Za-zА-Яа-яЁё]+$/.test(formContactsInputName.value)&&formContactsInputName.setCustomValidity("В имени должны быть только буквы"),formContactsInputName.reportValidity()};null!==formContactsInput&&formContactsInputName.addEventListener("input",checkNumbers);const formContactsEmail=document.getElementById("email");function ValidateEmail(t){return!!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(t)}const checkMail=()=>{ValidateEmail(formContactsEmail.value)?formContactsEmail.setCustomValidity(""):formContactsEmail.setCustomValidity("адрес почты неверный"),formContactsEmail.reportValidity()};null!==formContactsInput&&formContactsEmail.addEventListener("input",checkMail);const burgerToggle=document.querySelector(".main-nav__toggle"),burgerMenu=document.querySelector(".main-nav"),overlay=document.querySelector(".overlay"),changeToggle=()=>{null!==burgerMenu&&burgerMenu.classList.contains("main-nav--closed")?(burgerMenu.classList.add("main-nav--opened"),burgerMenu.classList.remove("main-nav--closed"),overlay.classList.remove("hidden"),overlay.classList.add("active"),body.classList.add("overflow-hidden")):(burgerMenu.classList.remove("main-nav--opened"),burgerMenu.classList.add("main-nav--closed"),overlay.classList.add("hidden"),overlay.classList.remove("active"),body.classList.remove("overflow-hidden"))};null!==formContactsInput&&burgerToggle.addEventListener("click",changeToggle);const burgerMenuItemList=document.querySelectorAll(".main-nav__item"),burgerMenuItemClick=()=>{null===burgerMenu||burgerMenu.classList.contains("main-nav--closed")||(burgerMenu.classList.remove("main-nav--opened"),burgerMenu.classList.add("main-nav--closed"),overlay.classList.add("hidden"),overlay.classList.remove("active"),body.classList.remove("overflow-hidden"))};burgerMenuItemList.forEach((t=>{t.addEventListener("click",burgerMenuItemClick)}));const hideModalForm=(t,e,n)=>{null!==t&&t.classList.contains(e)};function saveForm(t){const e=t.id,n=new FormData(t),a={};n.forEach((function(t,e){a[e]=t}));const o=JSON.stringify(a);localStorage.setItem("form["+e+"]",o)}function syncForm(t){document.getElementById(t).addEventListener("submit",(function(t){saveForm(t.target)}))}document.addEventListener("click",(t=>{var e,n;"main-nav"!=t.target.className&&"main-nav__toggle"!=t.target.className&&(e=document.querySelector(".main-nav"),n="main-nav--closed",null!==e&&e.classList.contains(n))})),syncForm("registration");const body=document.body;overlay.addEventListener("click",(function(t){overlay.classList.contains("active")&&(burgerMenu.classList.remove("main-nav--opened"),burgerMenu.classList.add("main-nav--closed"),overlay.classList.add("hidden"),overlay.classList.remove("active"),body.classList.remove("overflow-hidden"))}));
+import './vendor.js';
+//**  tel validation **//
+const formContactsInput = document.getElementById('telephone');
+
+const checkNumber = () => {
+
+  const minTitleName = 10;
+  const maxTitleName = 12;
+
+  const valueLength = formContactsInput.value.length;
+  if (valueLength < minTitleName) {
+    formContactsInput.setCustomValidity(`Ещё ${minTitleName - valueLength} симв.`);
+  } else if (valueLength > maxTitleName) {
+    formContactsInput.setCustomValidity(`Удалите лишние ${valueLength - maxTitleName} симв.`);
+  } else if (!Number.isInteger(+formContactsInput.value)) {
+    formContactsInput.setCustomValidity(`Номер должен быть числом`);
+  }
+  else {
+    formContactsInput.setCustomValidity('');
+  }
+
+  formContactsInput.reportValidity();
+};
+
+if (formContactsInput !== null) {
+  formContactsInput.addEventListener('input', checkNumber);
+};
+
+
+//**  name validation **//
+const formContactsInputName = document.getElementById('name');
+
+const checkNumbers = () => {
+
+  const minTitleName = 1;
+  const maxTitleName = 45;
+
+  const valueLength = formContactsInputName.value.length;
+
+  formContactsInputName.setCustomValidity('');
+
+  if (valueLength > 0 && valueLength < minTitleName) {
+    formContactsInputName.setCustomValidity(`Ещё ${minTitleName - valueLength} симв.`);
+  } else if (valueLength > maxTitleName) {
+    formContactsInputName.setCustomValidity(`Удалите лишние ${valueLength - maxTitleName} симв.`);
+  }
+
+  if (valueLength>0 && !/^[ A-Za-zА-Яа-яЁё]+$/.test(formContactsInputName.value))
+  {
+    formContactsInputName.setCustomValidity(`В имени должны быть только буквы`);
+  }
+
+  formContactsInputName.reportValidity();
+};
+
+if (formContactsInput !== null) {
+  formContactsInputName.addEventListener('input', checkNumbers);
+};
+
+//**  email validation **//
+const formContactsEmail = document.getElementById('email');
+
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return (true)
+  }
+    return (false)
+}
+
+const checkMail = () => {
+
+  if (!ValidateEmail(formContactsEmail.value)) {
+    formContactsEmail.setCustomValidity(`адрес почты неверный`);
+  }
+  else {
+    formContactsEmail.setCustomValidity('');
+  }
+
+  formContactsEmail.reportValidity();
+};
+
+if (formContactsInput !== null) {
+  formContactsEmail.addEventListener('input', checkMail);
+};
+
+//** burger **//
+
+const burgerToggle = document.querySelector('.main-nav__toggle');
+const burgerMenu = document.querySelector('.main-nav');
+const overlay = document.querySelector('.overlay');
+
+const changeToggle = () => {
+
+  if (burgerMenu !== null && burgerMenu.classList.contains('main-nav--closed')) {
+    burgerMenu.classList.add('main-nav--opened');
+    burgerMenu.classList.remove('main-nav--closed');
+    overlay.classList.remove('hidden');
+    overlay.classList.add('active');
+    body.classList.add('overflow-hidden');
+
+  } else {
+    burgerMenu.classList.remove('main-nav--opened');
+    burgerMenu.classList.add('main-nav--closed');
+    overlay.classList.add('hidden');
+    overlay.classList.remove('active');
+    body.classList.remove('overflow-hidden');
+
+  }
+};
+
+if (formContactsInput !== null) {
+  burgerToggle.addEventListener('click', changeToggle);
+}
+
+// *menu *//
+const burgerMenuItemList = document.querySelectorAll('.main-nav__item');
+
+const burgerMenuItemClick = () => {
+
+  if (burgerMenu !== null && !burgerMenu.classList.contains('main-nav--closed')) {
+    burgerMenu.classList.remove('main-nav--opened');
+    burgerMenu.classList.add('main-nav--closed');
+    overlay.classList.add('hidden');
+    overlay.classList.remove('active');
+    body.classList.remove('overflow-hidden');
+  }
+  //hideModalForm(burgerMenu, 'main-nav--closed','main-nav--opened')
+};
+
+burgerMenuItemList.forEach(btn => {
+  btn.addEventListener('click', burgerMenuItemClick);
+});
+
+// ***
+const hideModalForm = (modalForm, classHideAdd, classHideRemove) => {
+  if (modalForm !== null && !modalForm.classList.contains(classHideAdd)) {
+  }
+};
+
+document.addEventListener('click', (event) => {
+
+  if ( event.target.className != 'main-nav' && event.target.className != 'main-nav__toggle') {
+    hideModalForm(document.querySelector('.main-nav'), 'main-nav--closed','main-nav--opened');
+  }
+});
+
+//* form *//
+function saveForm(form) {
+  const formName = form.id
+  const formData = new FormData(form)
+  const object = {}
+
+  formData.forEach(function(value, key) {
+    object[key] = value
+    })
+
+  const json = JSON.stringify(object)
+  localStorage.setItem('form['+ formName +']', json)
+  }
+
+  function syncForm(formName) {
+    const form = document.getElementById(formName)
+
+    form.addEventListener('submit', function(event) {
+      saveForm(event.target)
+  })
+  }
+
+  syncForm('registration')
+
+  //** overlay */
+  const body = document.body;
+
+  overlay.addEventListener('click', function(e) {
+
+    if (overlay.classList.contains('active')){
+
+      burgerMenu.classList.remove('main-nav--opened');
+      burgerMenu.classList.add('main-nav--closed');
+      overlay.classList.add('hidden');
+      overlay.classList.remove('active');
+      body.classList.remove('overflow-hidden');
+  };
+});
